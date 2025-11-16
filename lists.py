064bff8460bbc58f6fc5742bfacd65438e1aa8a7
Michael Myers <michael.myers@wustl.edu>
2025-11-16 09:35:16 -0600

    brought in code from my talon-personal

---
+++ lists.py
@@ -0,0 +1,6 @@
+from talon import Module, Context
+
+mod = Module()
+mod.list("organteq_manual", desc="Organteq manuals from bottom to top, numbers 1 through 4")
+mod.list("organteq_stop_number", desc="Organteq stop numbers from one to twenty")
+
